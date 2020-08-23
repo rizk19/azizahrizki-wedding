4118a77fb70601a7d468f475b346b57fda4b5679
rizki <rizki19@github.com>
2020-08-23 22:22:35 +0700

    Updates

---
+++ precache-manifest.dd7e0c4fd4596908b5ea061000e2e485.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942992d38199d3ae0e2957ef1eedd47c",
+    "url": "/azizahrizki-wedding/index.html"
+  },
+  {
+    "revision": "61ddaa047f9082183b1f",
+    "url": "/azizahrizki-wedding/static/css/main.a7ac2289.chunk.css"
+  },
+  {
+    "revision": "9de9493fce6cdf839a51",
+    "url": "/azizahrizki-wedding/static/js/2.1a9f4e71.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/azizahrizki-wedding/static/js/2.1a9f4e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ddaa047f9082183b1f",
+    "url": "/azizahrizki-wedding/static/js/main.09ce540f.chunk.js"
+  },
+  {
+    "revision": "09142e0c1581d4e26ae8",
+    "url": "/azizahrizki-wedding/static/js/runtime-main.45337c75.js"
+  },
+  {
+    "revision": "1ea00c44eea97107e85c8f264baaef4d",
+    "url": "/azizahrizki-wedding/static/media/1zizah-cropped.1ea00c44.JPG"
+  },
+  {
+    "revision": "115e33fbe3afb6ddde48d3166dfc3cf0",
+    "url": "/azizahrizki-wedding/static/media/DSC01189.115e33fb.JPG"
+  },
+  {
+    "revision": "7112982fcd0c359f6d4086cf5c98750d",
+    "url": "/azizahrizki-wedding/static/media/eki.7112982f.jpg"
+  },
+  {
+    "revision": "a69faa2e003f763a5afee6f9e99fb512",
+    "url": "/azizahrizki-wedding/static/media/frame-transparent-greeny-1side.a69faa2e.png"
+  }
+]);
