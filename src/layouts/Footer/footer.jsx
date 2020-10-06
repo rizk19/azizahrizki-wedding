a8e082535cfa8211650c8c3536f48819f7045d0d
rizki <rizki19@github.com>
2020-10-06 09:27:12 +0700

    footer change

--- src/layouts/Footer/footer.jsx
+++ src/layouts/Footer/footer.jsx
@@ -5,8 +5,13 @@ const Footer = () => {
 
     return (
         <footer className="footer">
-            <p className="footer-title">Template Originally Created by <a className="footer-link" href="https://github.com/rizk19"><strong>Rizki Ahmad Faris</strong></a>
-            </p>
+            <div className="img-footer"></div>
+            <div className="footer-entity">
+                <p className="footer-title">&copy;{' '}<a className="footer-link" href="https://github.com/rizk19"><strong>Rizki Ahmad Faris</strong></a> .2020</p>
+                <p className="footer-title"><a className="footer-link" href="https://github.com/rizk19">rizk19.github.io</a></p>
+                <caption className="footer-caption" >Created with love. By designers and developers who
+        love to work together.</caption>
+            </div>
         </footer>
     )
 }
